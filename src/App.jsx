e2bb5dde4dec3da2a1d232045f535b98c5c4dae6
finn-01 <nguyensam576@gmail.com>
2022-02-03 12:11:10 +0700

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import UserComponent from "./components/UserComponent";
+
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div className="App">
+			<h1 className="display-3 text-center">React With Sping</h1>
+			<UserComponent />;
+		</div>
+	);
 };
 
 export default App;
