4a9ce14c022af3f450647f06d97991adc9f0194d
finn-01 <nguyensam576@gmail.com>
2022-02-03 14:55:20 +0700

    add heroku server

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const USERS_REST_API_URL = "http://localhost:8080/api/users";
+const USERS_REST_API_URL =
+	"https://springboot-heroku-t.herokuapp.com/api/users";
 
 class UserService {
 	getUsers() {
